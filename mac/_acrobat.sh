7362f9169a27360492602b2d3edc6354d22980e5
AMJones <am@jonesiscoding.com>
2022-09-16 08:31:28 -0500

    Adds documentation for _acrobat.sh module

--- mac/_acrobat.sh
+++ mac/_acrobat.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+# /*
+#   Module:
+#     Contains functions that work with Adobe Acrobat DC, Reader, and XI
+#
+#   Example:
+#     source "<path-to-mac-libs>/mac/_acrobat.sh"
+#     <various code>
+#     <see function examples>
+#
+#   Copyright:
+#     © 2022/09 AMJones <am@jonesiscoding.com>
+#   License:
+#     For the full copyright and license information, please view the LICENSE
+#     file that was distributed with this source code.
+# */
+
 # Prevent being sourced more than once
 [ "${BASH_SOURCE[0]}" != "$0" ] && [ -n "$sourced_lib_mac_acrobat" ] && return 0
 
+# /*!
+#   Public: Displays the path to the given edition of Adobe Acrobat, if installed.
+#
+#   Example:
+#     acrobatPath=$(acrobat::path DC)
+#
+#   $1  The edition of Adobe Acrobat to retrieve the path for: DC, XI, or Reader.
+# */
 function acrobat::path() {
   local edition aDir
 
@@ -26,6 +50,15 @@ function acrobat::path() {
   return 1
 }
 
+# /*!
+#   Public: Displays the path to the current handler of PDF files for the OS.
+#
+#   Example:
+#     pdfHandler=$(acrobat::getPdfHandler)
+#
+#   Dependency:
+#     duti (https://github.com/moretension/duti)
+# */
 function acrobat::getPdfHandler() {
   local pathDuti edition aPath bundleId
 
@@ -39,6 +72,18 @@ function acrobat::getPdfHandler() {
   "$pathDuti" -x "com.adobe.pdf" | grep ".app" | tail -1
 }
 
+# /*!
+#   Public: Sets the PDF handler for the OS to the given edition of Adobe
+#   Acrobat, if installed.
+#
+#   Example:
+#     acrobat::setPdfHandler DC
+#
+#   Dependency:
+#     duti (https://github.com/moretension/duti)
+#
+#   $1    The edition of Adobe Acrobat to use for PDF files, if installed.
+# */
 function acrobat::setPdfHandler() {
   local pathDuti edition aPath bundleId
 
@@ -56,6 +101,14 @@ function acrobat::setPdfHandler() {
   fi
 }
 
+# /*!
+#   Public: Displays the installed version of the given edition of Adobe Acrobat.
+#
+#   Example:
+#     acrobatVersion=$(acrobat::version DC)
+#
+#   $1  The edition of Adobe Acrobat to retrieve the version for: DC, XI, or Reader.
+# */
 function acrobat::version() {
   local edition aPath
 
@@ -70,6 +123,18 @@ function acrobat::version() {
   return 1
 }
 
+# /*!
+#   Public: Runs the uninstaller for the given edition of Adobe Acrobat, if installed.
+#
+#   Example:
+#     if acrobat::uninstall XI; then
+#       <success code here>
+#     else
+#       <failure code here>
+#     fi
+#
+#   $1  The edition of Adobe Acrobat to uninstall: DC, XI, or Reader.
+# */
 function acrobat::uninstall() {
   local edition
 
@@ -100,7 +165,9 @@ function acrobat::uninstall() {
   fi
 }
 
-
+#
+# Initialization Code
+#
 if [ -z "$sourced_lib_mac_acrobat" ]; then
   # shellcheck disable=SC2034
   sourced_lib_mac_acrobat=0
