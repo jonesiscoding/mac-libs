66cb55712cf93146e457d3abf95686104e70fb0f
AMJones <am@jonesiscoding.com>
2024-06-10 11:18:55 -0500

    Improves os::version::name logic

--- os/version/name.sh
+++ os/version/name.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
-if [[ $(type -t "os::version::major") != function ]]; then
-  # shellcheck source=./major.sh disable=SC2164
-  source "$( cd "$(/usr/bin/dirname "${BASH_SOURCE[0]}")" ; /bin/pwd -P )/major.sh"
-fi
-
-if [[ $(type -t "os::version::minor") != function ]]; then
-  # shellcheck source=./minor.sh disable=SC2164
-  source "$( cd "$(/usr/bin/dirname "${BASH_SOURCE[0]}")" ; /bin/pwd -P )/minor.sh"
-fi
-
+# @description Outputs the friendly name for the macOS version
+# @stdout macOS Version Name
 function os::version::name() {
-  local MAJOR
-  local MINOR
+  local name license
 
-  MAJOR=$(os::version::major)
-  if [ "$MAJOR" -eq "10" ]; then
-    MINOR=$(os::version::minor)
-    [ "${MINOR:-0}" -eq "5" ] && echo "Leopard" && return 0
-    [ "${MINOR:-0}" -eq "6" ] && echo "Snow Leopard" && return 0
-    [ "${MINOR:-0}" -eq "7" ] && echo "Lion" && return 0
-    [ "${MINOR:-0}" -eq "8" ] && echo "Mountain Lion" && return 0
-    [ "${MINOR:-0}" -eq "9" ] && echo "Mavericks" && return 0
-    [ "${MINOR:-0}" -eq "10" ] && echo "Yosemite" && return 0
-    [ "${MINOR:-0}" -eq "11" ] && echo "El Capitan" && return 0
-    [ "${MINOR:-0}" -eq "12" ] && echo "Sierra" && return 0
-    [ "${MINOR:-0}" -eq "13" ] && echo "High Sierra" && return 0
-    [ "${MINOR:-0}" -eq "14" ] && echo "Mojave" && return 0
-    [ "${MINOR:-0}" -eq "15" ] && echo "Catalina" && return 0
+  # From the License
+  license="/System/Library/CoreServices/Setup Assistant.app/Contents/Resources/en.lproj/OSXSoftwareLicense.rtf'"
+  if [ -f "$license" ]; then
+    name=$(/usr/bin/awk '/SOFTWARE LICENSE AGREEMENT FOR macOS/' "$license" | awk -F 'macOS ' '{print $NF}' | awk '{print substr($0, 0, length($0)-1)}')
+    [ -n "$name" ] && echo "$name" && return 0
   fi
 
-  [ "$MAJOR" -eq "11" ] && echo "Big Sur" && return 0
-  [ "$MAJOR" -eq "12" ] && echo "Monterey" && return 0
-  [ "$MAJOR" -eq "13" ] && echo "Ventura" && return 0
-  [ "$MAJOR" -eq "14" ] && echo "Sonoma" && return 0
-
-  echo "Unknown" && return 0
+  # Backup in case the method above starts failing
+  case $(/usr/bin/sw_vers -productVersion) in
+    10.5.* ) echo "Leopard" && return 0        ;;
+    10.6.* ) echo "Snow Leopard" && return 0   ;;
+    10.7.* ) echo "Lion" && return 0           ;;
+    10.8.* ) echo "Mountain Lion" && return 0  ;;
+    10.9* ) echo "Mavericks" && return 0       ;;
+    10.10* ) echo "Yosemite" && return 0       ;;
+    10.11* ) echo "El Capitan" && return 0     ;;
+    10.12* ) echo "Sierra" && return 0         ;;
+    10.13* ) echo "High Sierra" && return 0    ;;
+    10.14* ) echo "Mojave" && return 0         ;;
+    10.15* ) echo "Catalina" && return 0       ;;
+    11.* )   echo "Big Sur" && return 0        ;;
+    12.* )   echo "Monterey" && return 0       ;;
+    13.* )   echo "Ventura" && return 0        ;;
+    14.* )   echo "Sonoma" && return 0         ;;
+    * )      echo "Unknown" && return 1        ;;
+  esac
 }
